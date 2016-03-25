2fab50bbfc8e0be5e5cd6ec32c4537664fd8335a
Josef Reichelt <nanomesher@gmail.com>
2016-03-25 12:32:18 +0100

    Basic bat movement and ball behavior
    Adds movement of the player bat and basic ball bounce

---
+++ Assets/Scripts/ballController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballController : MonoBehaviour
+{
+    public float ballSpeed = 5f;
+    private Rigidbody2D rigidBall;
+    private RaycastHit2D rayHit;
+
+    void Start()
+    {
+        rigidBall = this.GetComponent<Rigidbody2D>();
+        rigidBall.velocity = new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+
+
+    void FixedUpdate()
+    {
+
+
+        //Speed limit
+        if (rigidBall.velocity.magnitude < ballSpeed || rigidBall.velocity.magnitude > ballSpeed)
+        {
+            rigidBall.velocity = rigidBall.velocity.normalized * ballSpeed;
+        }
+
+        UpdateHitMarker();
+
+
+    }
+
+    void UpdateHitMarker()
+    {
+      rayHit = Physics2D.Raycast(transform.position,  rigidBall.velocity);
+      if (rayHit.collider != null && rayHit.collider.tag != "Ball")
+        {
+
+            Debug.DrawLine(transform.position, rayHit.point, Color.blue, 10f);
+            Debug.DrawRay(rayHit.point,Vector2.Reflect(rigidBall.velocity, rayHit.normal), Color.green);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Randomize bump angle
+        rigidBall.AddForce(rigidBall.velocity.normalized + new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f)));
+        if (col.gameObject.tag == "Brick")
+        {
+            Destroy(col.gameObject);
+        }
+
+    }
+
+
+
+
+    void OnDrawGizmos()
+    {
+      Gizmos.color = Color.green;
+      Gizmos.DrawWireSphere(rayHit.point, 0.2f);
+
+    }
+}
---
+++ Assets/Scripts/batController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class batController : MonoBehaviour
+{
+    public float batSpeed = 5f;
+    private Rigidbody2D rigio;
+
+  void Start()
+  {
+    rigio = this.GetComponent<Rigidbody2D>();
+  }
+
+
+  void FixedUpdate()
+   {
+    rigio.velocity = Vector2.up*Input.GetAxis("Vertical")*batSpeed;
+
+  }
+}
